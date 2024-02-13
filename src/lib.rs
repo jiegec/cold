c59accde58e1ce587bc44bb890a5d653c48df4a6
Jiajie Chen <c@jia.je>
2024-02-13 00:40:06 -0800

    Add test for push/pop state

--- src/lib.rs
+++ src/lib.rs
@@ -61,23 +61,33 @@ pub fn parse_opts(args: &Vec<String>) -> anyhow::Result<Opt> {
     let mut iter = args.iter();
     while let Some(arg) = iter.next() {
         match arg.as_str() {
-            "-plugin" => {
-                // skip plugin argument
-                iter.next();
-            }
-            "-m" => {
-                // emulation argument
-                opt.emulation = Some(
-                    iter.next()
-                        .ok_or(anyhow!("Missing emulation after -m"))?
-                        .to_string(),
-                );
+            // single dash
+            s @ _ if s.starts_with("-L") => {
+                // library search path argument
+                opt.search_dir
+                    .push(s.strip_prefix("-L").unwrap().to_string());
             }
             "-dynamic-linker" => {
                 // dynamic linker argument
                 opt.dynamic_linker = Some(
                     iter.next()
                         .ok_or(anyhow!("Missing dynamic linker after -dynamic-linker"))?
+                        .to_string(),
+                );
+            }
+            s @ _ if s.starts_with("-l") => {
+                // library argument
+                opt.obj_file.push(ObjFileOpt::Library(LibraryOpt {
+                    name: s.strip_prefix("-l").unwrap().to_string(),
+                    as_needed: cur_opt_stack.as_needed,
+                    link_static: cur_opt_stack.link_static,
+                }));
+            }
+            "-m" => {
+                // emulation argument
+                opt.emulation = Some(
+                    iter.next()
+                        .ok_or(anyhow!("Missing emulation after -m"))?
                         .to_string(),
                 );
             }
@@ -89,39 +99,23 @@ pub fn parse_opts(args: &Vec<String>) -> anyhow::Result<Opt> {
                         .to_string(),
                 );
             }
-            s @ _ if s.starts_with("-L") => {
-                // library search path argument
-                opt.search_dir
-                    .push(s.strip_prefix("-L").unwrap().to_string());
-            }
-            s @ _ if s.starts_with("-l") => {
-                // library argument
-                opt.obj_file.push(ObjFileOpt::Library(LibraryOpt {
-                    name: s.strip_prefix("-l").unwrap().to_string(),
-                    as_needed: cur_opt_stack.as_needed,
-                    link_static: cur_opt_stack.link_static,
-                }));
-            }
-            "--start-group" => {
-                opt.obj_file.push(ObjFileOpt::StartGroup);
-            }
-            "--end-group" => {
-                opt.obj_file.push(ObjFileOpt::EndGroup);
-            }
             "-pie" => {
                 opt.pie = true;
+            }
+            "-plugin" => {
+                // skip plugin argument
+                iter.next();
+            }
+            s @ _ if s.starts_with("-plugin-opt=") => {
+                // ignored
             }
             "-static" => {
                 cur_opt_stack.link_static = true;
             }
+
+            // double dashes
             "--as-needed" => {
                 cur_opt_stack.as_needed = true;
-            }
-            "--push-state" => {
-                opt_stack.push(cur_opt_stack);
-            }
-            "--pop-state" => {
-                cur_opt_stack = opt_stack.pop().unwrap();
             }
             "--build-id" => {
                 opt.build_id = true;
@@ -129,9 +123,19 @@ pub fn parse_opts(args: &Vec<String>) -> anyhow::Result<Opt> {
             "--eh-frame-hdr" => {
                 opt.eh_frame_hdr = true;
             }
-            s @ _ if s.starts_with("-plugin-opt=") => {
-                // ignored
+            "--end-group" => {
+                opt.obj_file.push(ObjFileOpt::EndGroup);
             }
+            "--start-group" => {
+                opt.obj_file.push(ObjFileOpt::StartGroup);
+            }
+            "--pop-state" => {
+                cur_opt_stack = opt_stack.pop().unwrap();
+            }
+            "--push-state" => {
+                opt_stack.push(cur_opt_stack);
+            }
+            // end of known flags
             s @ _ if s.starts_with("-") => {
                 // unknown flag
                 return Err(anyhow!("Unknown argument: {s}"));
@@ -143,4 +147,43 @@ pub fn parse_opts(args: &Vec<String>) -> anyhow::Result<Opt> {
         }
     }
     Ok(opt)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_push_pop_state() {
+        let opts = parse_opts(&vec![
+            "-llib1".to_string(),
+            "--push-state".to_string(),
+            "--as-needed".to_string(),
+            "-llib2".to_string(),
+            "--pop-state".to_string(),
+            "-llib3".to_string(),
+        ])
+        .unwrap();
+
+        assert_eq!(opts.obj_file.len(), 3);
+        if let ObjFileOpt::Library(lib) = &opts.obj_file[0] {
+            assert_eq!(lib.name, "lib1");
+            assert_eq!(lib.as_needed, false);
+        } else {
+            assert!(false);
+        }
+
+        if let ObjFileOpt::Library(lib) = &opts.obj_file[1] {
+            assert_eq!(lib.name, "lib2");
+            assert_eq!(lib.as_needed, true);
+        } else {
+            assert!(false);
+        }
+
+        if let ObjFileOpt::Library(lib) = &opts.obj_file[2] {
+            assert_eq!(lib.name, "lib3");
+            assert_eq!(lib.as_needed, false);
+        } else {
+            assert!(false);
+        }
+    }
 }
