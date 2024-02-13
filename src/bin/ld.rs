48d32296331994bf5c71d9f65ca4528d6d991e65
Jiajie Chen <c@jia.je>
2024-02-12 20:09:47 -0800

    Refactor argument parsing

--- src/bin/ld.rs
+++ src/bin/ld.rs
@@ -1,13 +1,5 @@
-use anyhow::anyhow;
-use cold::{LibraryOpt, Opt};
+use cold::parse_opts;
 use log::info;
-
-/// handle --push-state/--pop-state
-#[derive(Debug, Copy, Clone)]
-struct OptStack {
-    /// --as-needed
-    pub as_needed: bool,
-}
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
@@ -16,83 +8,7 @@ fn main() -> anyhow::Result<()> {
     info!("launched with args: {:?}", args);
 
     // parse arguments
-    let mut opt = Opt::default();
-    let mut cur_opt_stack = OptStack { as_needed: false };
-    let mut opt_stack = vec![];
-    let mut iter = args.iter();
-    while let Some(arg) = iter.next() {
-        match arg.as_str() {
-            "-plugin" => {
-                // skip plugin argument
-                iter.next();
-            }
-            "-m" => {
-                // emulation argument
-                opt.emulation = Some(
-                    iter.next()
-                        .ok_or(anyhow!("Missing emulation after -m"))?
-                        .to_string(),
-                );
-            }
-            "-dynamic-linker" => {
-                // dynamic linker argument
-                opt.dynamic_linker = Some(
-                    iter.next()
-                        .ok_or(anyhow!("Missing dynamic linker after -dynamic-linker"))?
-                        .to_string(),
-                );
-            }
-            "-o" => {
-                // output argument
-                opt.output = Some(
-                    iter.next()
-                        .ok_or(anyhow!("Missing output after -o"))?
-                        .to_string(),
-                );
-            }
-            s @ _ if s.starts_with("-L") => {
-                // library search path argument
-                opt.search_dir
-                    .push(s.strip_prefix("-L").unwrap().to_string());
-            }
-            s @ _ if s.starts_with("-l") => {
-                // library argument
-                opt.obj_file.push(cold::ObjFileOpt::Library(LibraryOpt {
-                    name: s.strip_prefix("-l").unwrap().to_string(),
-                    as_needed: cur_opt_stack.as_needed,
-                }));
-            }
-            "-pie" => {
-                opt.pie = true;
-            }
-            "--as-needed" => {
-                cur_opt_stack.as_needed = true;
-            }
-            "--push-state" => {
-                opt_stack.push(cur_opt_stack);
-            }
-            "--pop-state" => {
-                cur_opt_stack = opt_stack.pop().unwrap();
-            }
-            "--build-id" => {
-                opt.build_id = true;
-            }
-            "--eh-frame-hdr" => {
-                opt.eh_frame_hdr = true;
-            }
-            s @ _ if s.starts_with("-plugin-opt=") => {
-                // ignored
-            }
-            s @ _ if s.starts_with("-") => {
-                // unknown flag
-                return Err(anyhow!("Unknown argument: {s}"));
-            }
-            s @ _ => {
-                // object file argument
-                opt.obj_file.push(cold::ObjFileOpt::File(s.to_string()));
-            }
-        }
-    }
+    let opt = parse_opts(&args);
 
     info!("parsed options: {opt:?}");
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+use anyhow::anyhow;
+
+/// handle --push-state/--pop-state
+#[derive(Debug, Copy, Clone)]
+struct OptStack {
+    /// --as-needed
+    pub as_needed: bool,
+}
+
 #[derive(Debug, Clone)]
 pub struct LibraryOpt {
     pub name: String,
@@ -10,7 +19,7 @@ pub enum ObjFileOpt {
     /// objfile
     File(String),
     /// -l namespec
-    Library(LibraryOpt)
+    Library(LibraryOpt),
 }
 
 #[derive(Debug, Clone, Default)]
@@ -30,5 +39,87 @@ pub struct Opt {
     /// -L searchdir
     pub search_dir: Vec<String>,
     /// objfile
-    pub obj_file: Vec<ObjFileOpt>
+    pub obj_file: Vec<ObjFileOpt>,
+}
+
+/// parse arguments
+pub fn parse_opts(args: &Vec<String>) -> anyhow::Result<Opt> {
+    let mut opt = Opt::default();
+    let mut cur_opt_stack = OptStack { as_needed: false };
+    let mut opt_stack = vec![];
+    let mut iter = args.iter();
+    while let Some(arg) = iter.next() {
+        match arg.as_str() {
+            "-plugin" => {
+                // skip plugin argument
+                iter.next();
+            }
+            "-m" => {
+                // emulation argument
+                opt.emulation = Some(
+                    iter.next()
+                        .ok_or(anyhow!("Missing emulation after -m"))?
+                        .to_string(),
+                );
+            }
+            "-dynamic-linker" => {
+                // dynamic linker argument
+                opt.dynamic_linker = Some(
+                    iter.next()
+                        .ok_or(anyhow!("Missing dynamic linker after -dynamic-linker"))?
+                        .to_string(),
+                );
+            }
+            "-o" => {
+                // output argument
+                opt.output = Some(
+                    iter.next()
+                        .ok_or(anyhow!("Missing output after -o"))?
+                        .to_string(),
+                );
+            }
+            s @ _ if s.starts_with("-L") => {
+                // library search path argument
+                opt.search_dir
+                    .push(s.strip_prefix("-L").unwrap().to_string());
+            }
+            s @ _ if s.starts_with("-l") => {
+                // library argument
+                opt.obj_file.push(ObjFileOpt::Library(LibraryOpt {
+                    name: s.strip_prefix("-l").unwrap().to_string(),
+                    as_needed: cur_opt_stack.as_needed,
+                }));
+            }
+            "-pie" => {
+                opt.pie = true;
+            }
+            "--as-needed" => {
+                cur_opt_stack.as_needed = true;
+            }
+            "--push-state" => {
+                opt_stack.push(cur_opt_stack);
+            }
+            "--pop-state" => {
+                cur_opt_stack = opt_stack.pop().unwrap();
+            }
+            "--build-id" => {
+                opt.build_id = true;
+            }
+            "--eh-frame-hdr" => {
+                opt.eh_frame_hdr = true;
+            }
+            s @ _ if s.starts_with("-plugin-opt=") => {
+                // ignored
+            }
+            s @ _ if s.starts_with("-") => {
+                // unknown flag
+                return Err(anyhow!("Unknown argument: {s}"));
+            }
+            s @ _ => {
+                // object file argument
+                opt.obj_file.push(ObjFileOpt::File(s.to_string()));
+            }
+        }
+    }
+    Ok(opt)
 }
