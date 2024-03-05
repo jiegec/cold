ecc4f3cedf67375fe5feb7dde864ceea520791ee
Jiajie Chen <c@jia.je>
2024-03-05 17:45:50 +0800

    Add two file example to test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -5,3 +5,7 @@ set -x
 RUST_LOG=info cargo run -- helloworld_asm.o -o helloworld_asm_cold
 ./helloworld_asm_cold
 [[ $(./helloworld_asm_cold) == "Hello world!" ]] || exit 1
+
+RUST_LOG=info cargo run -- helloworld2_asm1.o helloworld2_asm2.o -o helloworld2_asm_cold
+./helloworld2_asm_cold
+[[ $(./helloworld2_asm_cold) == "Hello world!" ]] || exit 1
