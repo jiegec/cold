2ca295c1758f706dbfb7c56161b52fec18ae1c6d
Jiajie Chen <c@jia.je>
2024-03-05 15:59:57 +0800

    Add gitlab ci

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 ./build.sh
+
 RUST_LOG=info cargo run -- helloworld_asm.o -o helloworld_asm_cold
 ./helloworld_asm_cold
+[[ $(./helloworld_asm_cold) == "Hello world!" ]] || exit 1
